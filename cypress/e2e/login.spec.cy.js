366f2261df55d92ab88120bdaf459c6894d8ad2d
cleyton-Gusmao <cleytongusmao18@gmail.com>
2025-03-06 13:03:07 -0300

    added user information

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,31 @@
+import userData from '../fixtures/userData.json'
+
+describe('Login Orange HRM', () => {
+
+  const selectorList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    dashboardGrid: ".orangehrm-dashboard-grid",
+    wrongCredentialAlert: ".oxd-alert"
+  }
+
+  it('Login - Fail', () => {
+    cy.visit('/auth/login')
+    cy.get(selectorList.usernameField).type(userData.userFail.username)
+    cy.get(selectorList.passwordField).type(userData.userFail.password)
+    cy.get(selectorList.loginButton).click()
+    cy.location('pathname').should('equal', '/web/index.php/auth/login')
+    cy.get(selectorList.wrongCredentialAlert)
+  })
+
+  it('Login - Successfully', () => { 
+    cy.visit('/auth/login')
+    cy.get(selectorList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorList.passwordField).type(userData.userSuccess.password)
+    cy.get(selectorList.loginButton).click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get(selectorList.dashboardGrid)
+  })
+
+})
--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -7,24 +7,20 @@ describe('Orange HRM Tests', () => {
     passwordField: "[name='password']",
     loginButton: "[type='submit']",
     dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: ".oxd-alert",
     myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
     firstNameField: "[name='firstName']",
     midlleNameField: "[name='middleName']",
     lastNameField: "[name='lastName']",
     genericFild: ".oxd-input--active",
     dateFild: "[placeholder='yyyy-dd-mm']",
-    dateCloseButton: ".--close"
+    dateCloseButton: ".--close",
+    selectorButton: ".oxd-select-text--active",
+    nationalitySelector: ":nth-child(27)",
+    maritalStatusSelector: ".oxd-select-dropdown > :nth-child(4)",
+    genderButton: ".oxd-radio-wrapper",
+    saveButton: ".oxd-button--secondary",
+    bloodTypeSelector: ".oxd-select-dropdown > :nth-child(6)"
   }
-
-  it('Login - Fail', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorList.usernameField).type(userData.userFail.username)
-    cy.get(selectorList.passwordField).type(userData.userFail.password)
-    cy.get(selectorList.loginButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/auth/login')
-    cy.get(selectorList.wrongCredentialAlert)
-  })
 
   it('User Info Ipdate', () => {
     cy.visit('/auth/login')
@@ -42,8 +38,21 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorList.genericFild).eq(3).clear().type("ACC1051")
     cy.get(selectorList.genericFild).eq(4).clear().type("Other Id")
     cy.get(selectorList.genericFild).eq(5).clear().type("Driver's License Number")
-    cy.get(selectorList.dateFild).eq(0).clear().type("2000-04-04")
+    cy.get(selectorList.dateFild).eq(0).clear().type("2026-04-04")
     cy.get(selectorList.dateCloseButton).click()
+    cy.get(selectorList.selectorButton).eq(0).click()
+    cy.get(selectorList.nationalitySelector).click()
+    cy.get(selectorList.selectorButton).eq(1).click()
+    cy.get(selectorList.maritalStatusSelector).click()
+    cy.get(selectorList.dateFild).eq(1).clear().type("2000-04-04")
+    cy.get(selectorList.dateCloseButton).click()
+    cy.get(selectorList.genderButton).eq(0).click()
+    cy.get(selectorList.saveButton).eq(0).click()
+    cy.get('body').should('contain', 'Successfully Updated')
+    cy.get(selectorList.selectorButton).eq(2).click()
+    cy.get(selectorList.bloodTypeSelector).click()
+    cy.get(selectorList.genericFild).eq(9).clear().type("Test_Field")
+    cy.get(selectorList.saveButton).eq(1).click()
   })
 
 })
